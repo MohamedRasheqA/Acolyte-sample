9d7b2d3434bf5e0c060eaa7a441b8113c5adf709
MohamedRasheqA <rasheq@klmsolutions.in>
2025-02-27 23:47:13 +0530

    First Commit

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-        ],
-      },
-    ];
+const nextConfig = {
+  experimental: {
+    instrumentationHook: true,
   },
-};
+} as NextConfig;
 
 export default nextConfig;
