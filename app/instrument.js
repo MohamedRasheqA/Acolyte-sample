de4f17b4e0edc64a65961b8d7e45d812aef61ad9
MohamedRasheqA <rasheq@klmsolutions.in>
2025-02-27 18:22:33 +0530

    Eight Commit

--- app/instrument.js
+++ app/instrument.js
@@ -1,9 +1,8 @@
 import { registerOTel } from "@vercel/otel";
-import { AISDKExporter } from "langsmith/vercel";
-
+import { AISDKExporter } from "langsmith/traceable";
 export function register() {
   registerOTel({
-    serviceName: "Drug-Pricing-Teach-Back",
+    serviceName: "Teach Back: Drug-statistics",
     traceExporter: new AISDKExporter(),
   });
 }
